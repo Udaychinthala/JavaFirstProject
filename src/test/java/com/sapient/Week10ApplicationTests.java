a6a0ec6bc3917cabd6cac2205768c93349a2223a
Uday Chinthala <udaychinthala99@gmail.com>
2020-06-16 07:22:01 +0530

    final commit

---
+++ src/test/java/com/sapient/Week10ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sapient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week10ApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
